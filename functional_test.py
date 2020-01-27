2f22b3d13fa437c3045e5f6de3ab1559c08ea163
sunhooo <sssunho.lee@gmail.com>
2020-01-27 18:31:38 +0900

    First commit: First FT and basic django config

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+#browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path="C:/Users/sssun/AppData/Local/Programs/Python/geckodriver.exe")
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
-
+#assert 'Django' in browser.title
+assert 'TEST' in browser.title
